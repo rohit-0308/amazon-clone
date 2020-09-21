8f231c7f86b58ae2159f3c66edb89e182752c01d
rohit-0308 <rohitkumar860311@gmail.com>
2020-09-21 12:46:34 +0530

    changed some files

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyD5qByRRXJnIQ5rS4GnZNfTe10C1_DaNcE",
